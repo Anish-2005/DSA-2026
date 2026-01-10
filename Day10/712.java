52ca8d4b51d934f1491d21749c8c09ab6ed2bcac
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2026-01-10 23:54:27 +0530

    Create 712.java

---
+++ Day10/712.java
@@ -0,0 +1,30 @@
+//712. Minimum ASCII Delete Sum for Two Strings
+
+//Given two strings s1 and s2, return the lowest ASCII 
+// sum of deleted characters to make two strings equal.
+
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(
+                        dp[i - 1][j] + s1.charAt(i - 1),
+                        dp[i][j - 1] + s2.charAt(j - 1)
+                    );
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
