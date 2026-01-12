52e79c2c097732627799f3de967fdfbd81dd1f1c
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2026-01-13 01:53:30 +0530

    Create 1323.java

---
+++ Day12/1323.java
@@ -0,0 +1,20 @@
+//1323. Maximum 69 Number
+
+//You are given a positive integer num consisting 
+//only of digits 6 and 9.
+
+//Return the maximum number you can get by changing at most 
+// one digit (6 becomes 9, and 9 becomes 6).
+
+class Solution {
+    public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
