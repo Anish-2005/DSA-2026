b07bae3e3c10b398a70efae731d6368967002030
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2026-02-16 02:44:29 +0530

    Create 67.java

---
+++ Day46/67.java
@@ -0,0 +1,19 @@
+//67. Add Binary
+
+//Given two binary strings a and b, return their sum as a binary string.
+
+ class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0) carry += a.charAt(i--) - '0';
+            if (j >= 0) carry += b.charAt(j--) - '0';
+            sb.append(carry % 2);
+            carry /= 2;
+        }
+
+        return sb.reverse().toString();
+    }
+}
