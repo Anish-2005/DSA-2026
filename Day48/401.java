499aca796ab09cfd997fc6d0cf70aaf464564693
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2026-02-18 01:11:44 +0530

    Create 401.java

---
+++ Day48/401.java
@@ -0,0 +1,19 @@
+//401. Binary Watch
+
+//A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+                if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {
+                    result.add(hour + ":" + (minute < 10 ? "0" : "") + minute);
+                }
+            }
+        }
+
+        return result;
+    }
+}
