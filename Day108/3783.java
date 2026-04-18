42bb83a5fe63f40285e0bbf35046cac806589235
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2026-04-18 20:57:27 +0530

    Create 3783.java

---
+++ Day108/3783.java
@@ -0,0 +1,26 @@
+/*3783. Mirror Distance of an Integer
+
+You are given an integer n.
+
+Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+
+Return an integer denoting the mirror distance of n​​​​​​​.
+
+abs(x) denotes the absolute value of x.
+
+ */
+
+class Solution {
+    public int mirrorDistance(int n) {
+        return Math.abs(n - reverse(n));
+    }
+
+    private int reverse(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return rev;
+    }
+}
