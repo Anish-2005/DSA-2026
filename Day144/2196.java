e9b1b9c3eb863b23a1579d15495f8db73bbcad40
Anish Seth <anishseth0510@gmail.com>
2026-06-07 21:44:40 +0530

    Create 2196.java

---
+++ Day144/2196.java
@@ -0,0 +1,71 @@
+/*
+2196. Create Binary Tree From Descriptions
+
+You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid.
+
+ 
+
+Example 1:
+
+
+Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+Output: [50,20,80,15,17,19]
+Explanation: The root node is the node with value 50 since it has no parent.
+The resulting binary tree is shown in the diagram. */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer, TreeNode> map = new HashMap<>();
+        HashSet<Integer> children = new HashSet<>();
+
+        for (int[] d : descriptions) {
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            map.putIfAbsent(parent, new TreeNode(parent));
+            map.putIfAbsent(child, new TreeNode(child));
+
+            TreeNode p = map.get(parent);
+            TreeNode c = map.get(child);
+
+            if (isLeft == 1) {
+                p.left = c;
+            } else {
+                p.right = c;
+            }
+
+            children.add(child);
+        }
+
+        for (int[] d : descriptions) {
+            int parent = d[0];
+            if (!children.contains(parent)) {
+                return map.get(parent);
+            }
+        }
+
+        return null;
+    }
+}
