c6446ab9b3be0a0d152d8542f716847bf45ec22f
Anish Seth <anishseth0510@gmail.com>
2026-06-08 22:12:16 +0530

    Create 2161.java

---
+++ Day145/2161.java
@@ -0,0 +1,65 @@
+/*
+2161. Partition Array According to Given Pivot
+
+You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+Every element less than pivot appears before every element greater than pivot.
+Every element equal to pivot appears in between the elements less than and greater than pivot.
+The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot, then pi < pj.
+Return nums after the rearrangement.
+
+ 
+
+Example 1:
+
+Input: nums = [9,12,5,10,14,3,10], pivot = 10
+Output: [9,5,3,10,10,12,14]
+Explanation: 
+The elements 9, 5, and 3 are less than the pivot so they are on the left side of the array.
+The elements 12 and 14 are greater than the pivot so they are on the right side of the array.
+The relative ordering of the elements less than and greater than pivot is also maintained. [9, 5, 3] and [12, 14] are the respective orderings. */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int idx = 0;
+
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[idx++] = num;
+            }
+        }
+
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[idx++] = num;
+            }
+        }
+
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[idx++] = num;
+            }
+        }
+
+        return ans;
+    }
+}
