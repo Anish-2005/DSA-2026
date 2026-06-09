e9cffac29d0fec7d52c7a41d9cdb0a50841bdf11
Anish Seth <anishseth0510@gmail.com>
2026-06-10 00:54:09 +0530

    Create 3689.java

---
+++ Day146/3689.java
@@ -0,0 +1,43 @@
+/*
+3689. Maximum Total Subarray Value I
+
+You are given an integer array nums of length n and an integer k.
+
+You need to choose exactly k non-empty subarrays nums[l..r] of nums. Subarrays may overlap, and the exact same subarray (same l and r) can be chosen more than once.
+
+The value of a subarray nums[l..r] is defined as: max(nums[l..r]) - min(nums[l..r]).
+
+The total value is the sum of the values of all chosen subarrays.
+
+Return the maximum possible total value you can achieve.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2], k = 2
+
+Output: 4
+
+Explanation:
+
+One optimal approach is:
+
+Choose nums[0..1] = [1, 3]. The maximum is 3 and the minimum is 1, giving a value of 3 - 1 = 2.
+Choose nums[0..2] = [1, 3, 2]. The maximum is still 3 and the minimum is still 1, so the value is also 3 - 1 = 2.
+Adding these gives 2 + 2 = 4.
+ */
+
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int mx = Integer.MIN_VALUE;
+        int mn = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            mx = Math.max(mx, num);
+            mn = Math.min(mn, num);
+        }
+
+        return 1L * k * (mx - mn);
+    }
+}
