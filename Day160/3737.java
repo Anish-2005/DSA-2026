c387ba55f7d407b15fa5ca9da05ea43914c0aa24
Anish Seth <anishseth0510@gmail.com>
2026-06-25 21:15:04 +0530

    Create 3737.java

---
+++ Day160/3737.java
@@ -0,0 +1,81 @@
+/*
+3737. Count Subarrays With Majority Element I
+
+You are given an integer array nums and an integer target.
+
+Return the number of subarrays of nums in which target is the majority element.
+
+The majority element of a subarray is the element that appears strictly more than half of the times in that subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3], target = 2
+
+Output: 5
+
+Explanation:
+
+Valid subarrays with target = 2 as the majority element:
+
+nums[1..1] = [2]
+nums[2..2] = [2]
+nums[1..2] = [2,2]
+nums[0..2] = [1,2,2]
+nums[1..3] = [2,2,3]
+So there are 5 such subarrays. */
+
+
+class Solution {
+    class Fenwick {
+        int[] bit;
+        int n;
+
+        Fenwick(int n) {
+            this.n = n;
+            bit = new int[n + 2];
+        }
+
+        void update(int idx, int val) {
+            while (idx <= n) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countSubarrays(int[] nums, int target) {
+        int n = nums.length;
+
+        int offset = n + 2;
+        Fenwick ft = new Fenwick(2 * n + 5);
+
+        long ans = 0;
+        int pref = 0;
+
+        ft.update(offset, 1); // prefix sum = 0
+
+        for (int x : nums) {
+            if (x == target)
+                pref++;
+            else
+                pref--;
+
+            ans += ft.query(pref + offset - 1);
+
+            ft.update(pref + offset, 1);
+        }
+
+        return ans;
+    }
+}
