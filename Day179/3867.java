acf2b2d0a519fb557bf88f53a073c53856fff83f
Anish Seth <anishseth0510@gmail.com>
2026-07-16 22:32:34 +0530

    Create 3867.java

---
+++ Day179/3867.java
@@ -0,0 +1,74 @@
+/*3867. Sum of GCD of Formed Pairs
+
+You are given an integer array nums of length n.
+
+Construct an array prefixGcd where for each index i:
+
+Let mxi = max(nums[0], nums[1], ..., nums[i]).
+prefixGcd[i] = gcd(nums[i], mxi).
+After constructing prefixGcd:
+
+Sort prefixGcd in non-decreasing order.
+Form pairs by taking the smallest unpaired element and the largest unpaired element.
+Repeat this process until no more pairs can be formed.
+For each formed pair, compute the gcd of the two elements.
+If n is odd, the middle element in the prefixGcd array remains unpaired and should be ignored.
+Return an integer denoting the sum of the GCD values of all formed pairs.
+
+The term gcd(a, b) denotes the greatest common divisor of a and b.
+ 
+
+Example 1:
+
+Input: nums = [2,6,4]
+
+Output: 2
+
+Explanation:
+
+Construct prefixGcd:
+
+i	nums[i]	mxi	prefixGcd[i]
+0	2	2	2
+1	6	6	6
+2	4	6	2
+prefixGcd = [2, 6, 2]. After sorting, it forms [2, 2, 6].
+
+Pair the smallest and largest elements: gcd(2, 6) = 2. The remaining middle element 2 is ignored. Thus, the sum is 2. */
+
+import java.util.*;
+
+class Solution {
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            mx = Math.max(mx, nums[i]);
+            prefixGcd[i] = gcd(nums[i], mx);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long ans = 0;
+        int l = 0, r = n - 1;
+
+        while (l < r) {
+            ans += gcd(prefixGcd[l], prefixGcd[r]);
+            l++;
+            r--;
+        }
+
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
