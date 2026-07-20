9b10910fb9e88b883e22f3ebff7969a5179e6f31
Anish Seth <anishseth0510@gmail.com>
2026-07-21 02:11:37 +0530

    Create 1260.java

---
+++ Day182/1260.java
@@ -0,0 +1,60 @@
+/*1260. Shift 2D Grid
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a 2D grid of size m x n and an integer k. You need to shift the grid k times.
+
+In one shift operation:
+
+Element at grid[i][j] moves to grid[i][j + 1].
+Element at grid[i][n - 1] moves to grid[i + 1][0].
+Element at grid[m - 1][n - 1] moves to grid[0][0].
+Return the 2D grid after applying shift operation k times.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+Output: [[9,1,2],[3,4,5],[6,7,8]] */
+
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int total = m * n;
+        k %= total;
+
+        int[][] ans = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int oldIndex = i * n + j;
+                int newIndex = (oldIndex + k) % total;
+
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                ans[newRow][newCol] = grid[i][j];
+            }
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(ans[i][j]);
+            }
+            res.add(row);
+        }
+
+        return res;
+    }
+}
