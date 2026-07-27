0ff6967ef5fda81dce88935a237768649582bb84
ANISH SETH <anishseth0510@gmail.com>
2026-07-27 21:15:06 +0530

    Create 1464.java

---
+++ Day186/1464.java
@@ -0,0 +1,34 @@
+/*1464. Maximum Product of Two Elements in an Array
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. */
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max1 = 0;
+        int max2 = 0;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
