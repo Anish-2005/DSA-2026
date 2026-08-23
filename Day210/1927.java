e71564e1500db73ea41ed5bfda1cf31ebfea8d20
Anish Seth <anishseth0510@gmail.com>
2026-08-24 02:07:39 +0530

    Create 1927.java

---
+++ Day210/1927.java
@@ -0,0 +1,58 @@
+/* 1927. Sum Game
+
+Alice and Bob take turns playing a game, with Alice starting first.
+
+You are given a string num of even length consisting of digits and '?' characters. On each turn, a player will do the following if there is still at least one '?' in num:
+
+Choose an index i where num[i] == '?'.
+Replace num[i] with any digit between '0' and '9'.
+The game ends when there are no more '?' characters in num.
+
+For Bob to win, the sum of the digits in the first half of num must be equal to the sum of the digits in the second half. For Alice to win, the sums must not be equal.
+
+For example, if the game ended with num = "243801", then Bob wins because 2+4+3 = 8+0+1. If the game ended with num = "243803", then Alice wins because 2+4+3 != 8+0+3.
+Assuming Alice and Bob play optimally, return true if Alice will win and false if Bob will win.
+
+ 
+
+Example 1:
+
+Input: num = "5023"
+Output: false
+Explanation: There are no moves to be made.
+The sum of the first half is equal to the sum of the second half: 5 + 0 = 2 + 3.*/
+
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = leftQ - rightQ;
+
+        return diff * 2 != -9 * qDiff;
+    }
+}
